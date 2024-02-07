3d907ae97c07d028c9283e8a9b536a6675a5058f
Rohit <rohitpojari36@gmail.com>
2024-02-07 22:10:40 +0530

    Update Main.java

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int a = sc.nextInt();
+        System.out.println("Enter another number ");
+        int b = sc.nextInt();
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int remainder = a % b;
+        int quotient = a / b;
+        System.out.println("sum  =" + sum );
+        System.out.println("remainder =" + remainder);
+        System.out.println("product =" + product);
+        System.out.println("difference =" + difference);
+        System.out.println("quotient =" + quotient);
     }
 }
