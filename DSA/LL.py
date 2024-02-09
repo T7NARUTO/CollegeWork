47b0b21dc82946f35cc45a7d9b57ebcb04ef628f
Rohit <rohitpojari36@gmail.com>
2024-02-09 20:28:52 +0530

    Update LL.py

--- DSA/LL.py
+++ DSA/LL.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self,value):
+    def __init__(self, value):
         self.data = value
         self.next = None
 
@@ -20,7 +20,18 @@ class LL:
     def __len__(self):
         return self.n
 
-    def insert_head(self,value):
+    def __getitem__(self, item):
+        pos = 0
+        cur = self.head
+        if item > self.n:
+            return "invalid index"
+        while cur is not None:
+            if pos == item:
+                return cur.data
+            pos += 1
+            cur = cur.next
+
+    def insert_head(self, value):
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
@@ -31,10 +42,10 @@ class LL:
             self.head = new_node
             self.n += 1
 
-    def insert_tail(self,value):
+    def insert_tail(self, value):
         new_node = Node(value)
         cur = self.head
-        if self.head == None:
+        if self.head is None:
             self.head = new_node
         while cur is not None:
             if cur.next is None:
@@ -46,9 +57,9 @@ class LL:
 
     def pop(self):
         cur = self.head
-        if cur == None:
+        if cur is None:
             return "empty ll"
-        if cur.next == None:
+        if cur.next is None:
             self.head = None
         while cur is not None:
             if cur.next.next is None:
@@ -57,19 +68,71 @@ class LL:
                 return
             cur = cur.next
 
-    def remove(self,value):
+    def remove(self, value):
         cur = self.head
         if cur.data == value:
             self.head = cur.next
             return
-        while cur.next is not None:
+        while cur is not None:
             if cur.next.data == value:
-                break
+                if cur.next.next is None:
+                    cur.next = None
+                    self.n -= 1
+                    return
+                cur.next = cur.next.next
+                self.n -= 1
+                return
             cur = cur.next
         return 'value not in ll'
+
+    def del_head(self):
+        cur = self.head
+        if cur is None:
+            return "empty LL"
+        if cur.next is None:
+            self.head = None
+            self.n -= 1
+            return
+        self.head = cur.next
+        self.n -= 1
+
+    def del_tail(self):
+        cur = self.head
+        if cur is None:
+            return "empty LL"
+        if cur.next is None:
+            self.head = None
+            return
+        while cur.next.next is not None:
+            cur = cur.next
+        cur.next = None
+        self.n -= 1
+
+    def index(self, value):
+        pos = 0
+        cur = self.head
+        while cur is not None:
+            if cur.data == value:
+                return pos
+            pos += 1
+            cur = cur.next
+        return "item not found"
+
+    def replace(self, prev_val, new_val):
+        cur = self.head
+        while cur is not None:
+            if cur.data == prev_val:
+                cur.data = new_val
+                return
+            cur = cur.next
+        return "value not found"
+
+
 ll = LL()
-l = [1,12,13,14,15,16,17,18,19,20]
-ll.insert_tail(11)
-ll.insert_tail(12)
-ll.remove(12)
+l = [1, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+ll.insert_head(1)
+ll.insert_head(10)
+ll.insert_head(11)
+ll.insert_head(12)
+ll.replace(10 ,2)
 print(ll)
