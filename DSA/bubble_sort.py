8f2b15121591069eb23410587c4b699074d2964b
Rohit <rohitpojari36@gmail.com>
2024-03-05 19:31:57 +0530

    created Bubble Sort and linear search

--- DSA/bubble_sort.py
+++ DSA/bubble_sort.py
@@ -1,17 +1,31 @@
-def Bubble_sort(arr):
-    len_arr = len(arr)
+def Bubble_sort(array):
+    len_arr = len(array)
     while len_arr > 0:
         i = 0
         while i < len_arr - 1:
-            if arr[i] > arr[i+1]:
-                temp = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = temp
+            if array[i] > array[i + 1]:
+                temp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = temp
             i += 1
         len_arr -= 1
-    return arr
+    return array
 
 
-arr = [11, 10, 1]
-Bubble_sort(arr)
-print(arr)
+while True:
+    print("1.Initialise the Array. "
+          "2.Sort the array using Bubble sort. "
+          "3.Exit. ")
+    options = int(input('Select a Option to continue. '))
+    if options == 1:
+        arr = []
+        n = int(input("Enter the size of the array. "))
+        for i in range(n):
+            element = int(input(f"Enter the {i} element of the array. "))
+            arr.append(element)
+        continue
+    elif options == 2:
+        Bubble_sort(arr)
+        print(arr)
+    elif options == 3:
+        break
--- DSA/linear_search.py
+++ DSA/linear_search.py
@@ -1,14 +1,28 @@
-def linear_search(arr,value):
-    len_arr = len(arr)
-    pos = None
+def linear_search(array, value):
+    len_arr = len(array)
     i = 0
     while i < len_arr:
-        if arr[i] == value:
+        if array[i] == value:
             pos = i
-            return print(f'element found at position {pos}')
+            return print(f'element found at position {pos}. ')
         i += 1
     return print('element not found')
 
 
-arr = [10,11,20,30]
-linear_search(arr,30)
+while True:
+    print("1.Initialise the Array. "
+          "2.Search for elements using Linear search. "
+          "3.Exit. ")
+    options = int(input('Select a Option to continue. '))
+    if options == 1:
+        arr = []
+        n = int(input("Enter the size of the array. "))
+        for i in range(n):
+            element = int(input(f"Enter the {i} element of the array. "))
+            arr.append(element)
+        continue
+    elif options == 2:
+        sc = int(input("Enter the Element to search in the array. "))
+        linear_search(arr, sc)
+    elif options == 3:
+        break
